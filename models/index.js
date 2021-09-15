7d7370556a73760d1b2730987b53e9785c81ce8d
Evan Doherty <edoherty77@gmail.com>
2021-09-14 20:53:06 -0400

    connected to atlas

--- models/index.js
+++ models/index.js
@@ -1,3 +1,21 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const connectionString =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/chatapp'
+const configOptions = {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  useUnifiedTopology: true,
+}
+
+mongoose
+  .connect(connectionString, configOptions)
+  .then(() => console.log('MongoDB successfully connected...'))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`))
+
+
 module.exports = {
   Chatroom: require('./chatroom'),
   Message: require('./message'),
--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 var express = require('express')
-var app = express()
 var cors = require('cors')
 var routes = require('./routes')
+var mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+require('dotenv').config({ path: '.env' })
 
+var app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
-app.use('/api/v1', routes.chatrooms)
+app.use(
+    session({
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/chatapp',
+      }),
+      secret: 'chatapp',
+      resave: false, // will not resave sessions
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+      },
+    }),
+  )
 
+  app.use('api/v1', routes.chatrooms)
 
-app.listen(4000, () => {
-    console.log("app is listening on port 4000")
-})
+//connection
+app.listen(process.env.PORT, () =>
+    console.log(`Server is running on port ${process.env.PORT}`),
+)   
